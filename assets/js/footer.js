9e5fd4cdabe7bb29536fd99652bbd1f1b7768cbb
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2026-01-16 22:24:44 +0100

    contact

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -30,6 +30,13 @@
 									'<ul class="menu">' +
 										'<li>&copy; <span id="copyright-year"></span> Gustavo García Bernal</li>' +
 										'<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>' +
+										'<li class="menu-icons">' +
+											'<ul class="icons footer-icons">' +
+												'<li><a href="mailto:gustavo.garciabernal@sciencespo.fr" class="icon solid fa-envelope"><span class="label">Email</span></a></li>' +
+												'<li><a href="https://www.linkedin.com/in/gustavojgb/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>' +
+												'<li><a href="https://github.com/tavojgb" class="icon brands fa-github"><span class="label">GitHub</span></a></li>' +
+											'</ul>' +
+										'</li>' +
 									'</ul>' +
 								'</div>' +
 							'</div>' +
